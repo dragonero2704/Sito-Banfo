a5c1f953ab38fdf8200518b3661c7dba4cab66fc
dragonero2704 <roberto.rudi04@gmail.com>
2022-10-25 12:06:53 +0200

    addauthor.php modified

--- ajax/addauthor.php
+++ ajax/addauthor.php
@@ -6,44 +6,42 @@ require_once('../data/db.php');
 $codice = $_GET['q'];
 
 $database = new Database();
-if(!empty($database->connerror)){
-    echo "<p>Errore di connessione ".$database->connerror['code'].":".$database->connerror['message']."</p>";
+if (!empty($database->connerror)) {
+    echo "<p>Errore di connessione " . $database->connerror['code'] . ":" . $database->connerror['message'] . "</p>";
 }
-// $conn = new mysqli($dbhost, $dbusername, $dbpassword, $dbname) or die("Connessione fallita, come te.");
 
-$query = "SELECT nome, cognome, professione, classe
-        FROM redazione
-        WHERE codice='$codice'
-        ";
-
-// $ris = $conn->query($query);
-// $conn->close();
 $ris = $database->getAllFrom('redazione', "
 codice = '$codice'
 ");
 
+if (empty($ris)) {
+    $database->close();
+    die("{}");
+}
 $row = $ris[0];
 
-$img_path = file_exists('../immagini/redazione/' . $row['nome'] . '_' . $row['cognome'] . '.jpg') ? '../immagini/redazione/' . $row['nome'] . '_' . $row['cognome'] . '.jpg' : '../immagini/redazione/user.jpg';
+$img_path = file_exists('../immagini/redazione/' . $row['nome'] . '_' . $row['cognome'] . '.jpg') ?
+    '../immagini/redazione/' . $row['nome'] . '_' . $row['cognome'] . '.jpg' :
+    '../immagini/redazione/user.jpg';
 
+$database->close();
 
-
-// json_encode(array(
+// die(json_encode(array(
 //     "img_path" => $img_path,
-//     "nome" => $nome,
-//     "congnome"=>$cognome,
-//     "codice"=>$codice,
-//     "professione"=>$row['professione'],
-//     "classe"=>$row['classe']
-// ));
-// echo '<div class="item" onclick="deselect(this)" id="'.$codice.'-display"><h1>'.$row['nome'].' '.$row['cognome'].'</h1></div>';
+//     "nome" => $row['nome'],
+//     "cognome" => $row['cognome'],
+//     "codice" => $codice,
+//     "professione" => $row['professione'],
+//     "classe" => $row['classe']
+// )))
+echo '<div class="item" onclick="deselect(this)" id="'.$codice.'-display"><h1>'.$row['nome'].' '.$row['cognome'].'</h1></div>';
 echo "<div class='Red_singolo-membro' id='".$codice."-display'>
-    
+
     <div class='Red_singolo_membro_img'>
         <img src='".$img_path."'>
     </div>
     <div class='delete_button' onclick='deselect(this.parentElement)'><span></span><span></span></div>
-    
+
     <div class='Red_contenitore_membro'>
         <h2>" . $row["nome"] . " " . $row["cognome"] . "</h2>
         <div class='Red_professione'><input type='text' name='".$codice."-ruolo' value='". $row["professione"] ."'></div>
