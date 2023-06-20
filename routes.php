d57dcc910847388376ad2358492ee910b5b6b092
dragonero2704 <roberto.rudi04@gmail.com>
2023-06-20 16:48:03 +0200

    updated routes.php

--- routes.php
+++ routes.php
@@ -8,32 +8,8 @@ $router->any("/redattore", "./pagine/redattore.php");
 $router->any("/login", "./pagine/login.php");
 $router->any("/logout", "./pagine/logout.php");
 $router->any("/news", "./pagine/nuoviarticoli.php");
-$router->any("/robots.txt", function () {
-    echo "Hai rotto il cazzo uam <br>";
-    echo "Daje Roma <br>";
-    echo "(Cojone)<br>";
 
-    function getIPAddress()
-    {
-        //whether ip is from the share internet  
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }
-        //whether ip is from the proxy  
-        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        }
-        //whether ip is from the remote address  
-        else {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-        return $ip;
-    }
-    $ip = getIPAddress();
-    echo 'Is this you? ' . $ip."<br>";
-    $logger = new Logger();
-    $logger->log("/robots.txt client ip: ".$ip);
-});
+
 $router->any("/argomento/{}", function ($argomento) {
     require_once("./pagine/argomento.php");
 });
@@ -60,6 +36,47 @@ $router->post("/api/author", function ($codice) {
 $router->post("/api/search/{}", function ($keyword) {
     require_once("./api/search.php");
 });
+
+
+//route per gli hackerini
+$router->any("/robots.txt", function () {
+    echo "Hai rotto il cazzo uam <br>";
+    echo "Daje Roma <br>";
+    echo "(Cojone)<br>";
+
+    function getIPAddress()
+    {
+        //whether ip is from the share internet  
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+        //whether ip is from the proxy  
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        //whether ip is from the remote address  
+        else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+    $ip = getIPAddress();
+    echo 'Is this you? ' . $ip."<br>";
+    $logger = new Logger();
+    $logger->log("/robots.txt client ip: ".$ip);
+});
+
+$router->any("/.env", function(){
+    echo "Bro non è in js <br>";
+    echo "(Cojone)<br>";
+
+    $ip = getIPAddress();
+    echo 'Is this you? ' . $ip."<br>";
+    $logger = new Logger();
+    $logger->log("/.env client ip: ".$ip);
+});
+
+
 
 // questa funzione smista le richieste HTTP
 // vedere file router.php
